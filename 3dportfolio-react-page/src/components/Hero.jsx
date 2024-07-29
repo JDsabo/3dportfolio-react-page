96550fd54bc9bbc06c788a55c00f9b997277b77b
JDsabo <82731778+JDsabo@users.noreply.github.com>
2024-07-29 15:55:48 -0600

    Update Hero.jsx

--- 3dportfolio-react-page/src/components/Hero.jsx
+++ 3dportfolio-react-page/src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Jose Saborio</span>
+            Hi, I'm <span className='text-[#915EFF]'>Jose D. Saborio</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
           FrontEnd Developer | UX/UI | Marketing Automation | 3D
